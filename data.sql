4a3a7e415df5069fbc983e86737b0406846a09b9
Juan Diego Guerra <jdxelric@gmail.com>
2023-09-19 22:12:51 -0500

    Update data.sql
    INSERT VALUES INTO animals' table

--- data.sql
+++ data.sql
@@ -1,6 +1,16 @@
 /* Populate database with sample data. */
-
+--DAY 01
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (1,'Agumon','2020-02-03',0,'t',10.23);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (2,'Gabumon','2018-11-15',2,'t',8.0);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3,'Pikachu','2021-01-07',1,'f',15.04);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (4,'Devimon','2017-05-12',5,'t',11.0);
+--DAY 02
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES 
+  (5,'Charmander','2020-02-08',0,'f', -11),
+  (6,'Plantmon','2021-11-15',2,'t', -5.7),
+  (7,'Squirtle','1993-04-02',3,'f', -12.13),
+  (8,'Angemon','2005-06-12',1,'t', -45),
+  (9,'Boarmon','2005-06-07',7,'t', 20.4),
+  (10,'Blossom','1998-10-13',3,'t', 17),
+  (11,'Ditto','2022-05-14',4,'t', 22);
